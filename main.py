25428b8e28d6f5e360648e1b8df3b79fbcaa60db
creature6969 <159709673+creature6969@users.noreply.github.com>
2024-11-22 03:45:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import config
 import time
 import logging
+from flask import Flask
 from pyrogram import Client, idle
 from pyromod import listen  # type: ignore
 from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
